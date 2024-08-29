6e1890aa3fe439a298d8deecbfa7cac6dea22a9a
rohan <advanirohan03@gmail.com>
2024-08-30 03:45:45 +0530

    Update

--- frontend/src/apiUrl.ts
+++ frontend/src/apiUrl.ts
@@ -1,6 +1,7 @@
-const baseUrl = 'http://localhost:8000';
+const baseUrl = 'http://localhost:4000';
 const version = "api/v1"
 
 export const GET_STOCK_DATA = `${baseUrl}/${version}/stocks/stockData`;
 export const GET_TOP_GAINERS = `${baseUrl}/${version}/stocks/top-gainers`;
 export const SEARCH_STOCKS = `${baseUrl}/${version}/stocks/search`;
+export const ANALYZE_STOCK = `${baseUrl}/${version}/stocks/analyze`;
--- frontend/src/components/Stock.tsx
+++ frontend/src/components/Stock.tsx
@@ -3,21 +3,21 @@ import { useState, useEffect } from 'react';
 import { StockPoint } from '@/types';
 import StockChart from './charts/chart';
 import axios from 'axios';
-import { GET_TOP_GAINERS, SEARCH_STOCKS } from '@/apiUrl';
+import { ANALYZE_STOCK, GET_TOP_GAINERS, SEARCH_STOCKS } from '@/apiUrl';
 
 export default function Stock() {
   const [data, setData] = useState<StockPoint[]>([]);
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const response = await axios.get(GET_TOP_GAINERS);
-  //    console.log(response.data)
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`${ANALYZE_STOCK}?symbol=GOOGL`);
+     console.log(response.data)
 
-  //   }
+    }
 
-  //   fetchData()
+    fetchData()
 
-  // }, []);
+  }, []);
 
 
 
