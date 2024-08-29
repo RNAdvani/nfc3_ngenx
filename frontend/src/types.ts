e035e0ba3ac69e80cb05591daaf5d1fc6a0ba3f6
Sahil Gurnani <gurnanisahil87@gmail.com>
2024-08-29 22:33:41 +0530

    Sidebar poora daal diya bc

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,31 +1,41 @@
+import { Icons } from "./components/iconss";
 
 export type IUser = {
-    email : string,
-    name : string,
-    password ?: string
+  email: string;
+  name: string;
+  password?: string;
+};
+
+export type UserRole = "admin" | "user";
+
+export interface NavItem {
+  title: string;
+  href?: string;
+  disabled?: boolean;
+  external?: boolean;
+  icon?: keyof typeof Icons;
+  label?: string;
+  description?: string;
 }
 
-export type UserRole = "admin" | "user"
-
 export interface StockQuote {
-    regularMarketPrice: {
-      raw: number;
-    };
-    regularMarketTime: {
-      raw: number;
-    };
-  }
-  
-  export interface StockData {
-    quoteSummary: {
-      result: {
-        price: StockQuote;
-      }[];
-    };
-  }
-  
-  export interface StockPoint {
-    date: number;
-    close: number;
-  }
-  
+  regularMarketPrice: {
+    raw: number;
+  };
+  regularMarketTime: {
+    raw: number;
+  };
+}
+
+export interface StockData {
+  quoteSummary: {
+    result: {
+      price: StockQuote;
+    }[];
+  };
+}
+
+export interface StockPoint {
+  date: number;
+  close: number;
+}
